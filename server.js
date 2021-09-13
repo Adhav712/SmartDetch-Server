9b358817f75dc5658abb74f4deebfa0110bea6fb
Adhav712 <Adhav712@gmail.com>
2021-09-13 22:17:43 +0530

    Testing Phase

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/',(req,res) =>{
+	res.send("success");
+})
+
+app.listen(3000,() => {
+	console.log("server running succesfully on port 3000")
+})
