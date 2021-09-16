55f9fb1b5b81197bd5c18400e7dd1d5b9f5ccacd
Adhav712 <Adhav712@gmail.com>
2021-09-16 21:50:51 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,98 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
+const database = {
+	users: [
+	{
+		id: '123',
+		name: 'Adhavan',
+		email: 'adhavan02@gmail.com',
+		password: 'adhavan07',
+		entries: 0,
+		joined: new Date()
+
+	},
+	{
+		id: '124',
+		name: 'john',
+		email: "johnsam@gmail.com",
+		password: 'john07',
+		entries: 0,
+		joined: new Date()	
+	}
+  ]
+}
 
 app.get('/',(req,res) =>{
-	res.send("success");
+	res.send(database.users);
 })
+
+app.post('/signin',(req,res) => {
+	if(req.body.email === database.users[0].email && 
+		req.body.password === database.users[0].password){
+		res.json('success')
+	}else{
+		res.status(404).json('error loggging In');
+	} 
+})
+
+
+app.post('/register',(req,res) =>{
+	const {name,email,password} = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1])
+})
+
+app.get('/profile/:id',(req,res) => {
+	const{id} = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+		    found = true;
+			return res.json(user);
+		}
+	})
+	if(!found){
+		res.status(404).json("no user founded")
+	}
+	
+})
+
+app.post('/image',(req,res) => {
+	const {id} = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if(user.id === id){
+			found = true;
+			user.entries++
+			return res.json(user.entries)
+		}
+	})
+	if(!found){
+		res.status(404).json("No user founded")
+	}
+})
+
+
 
 app.listen(3000,() => {
 	console.log("server running succesfully on port 3000")
 })
+
+
+/*
+/ --> res = this is working
+/signin --> POST = sucess/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image -->PUT --user
+*/
